4d74e4a84d487b8a6c403602577041b22bf6e2fa
SupaSebie <supasebie@gmail.com>
2022-09-21 20:55:18 -0700

    ngx bootstrap coors fa ssl

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,26 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var MyOrigins = "_myOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyOrigins,
+                        policy =>
+                        {
+                            policy.WithOrigins("https://localhost:4200")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                        });
+});
+
 // Add services to the container. Equivalent to startup
 // builder.Services.AddIdentityService(builder.configuration)
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer(); // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<DataContext>(options => {
+builder.Services.AddDbContext<DataContext>(options =>
+{
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
@@ -20,6 +34,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyOrigins);
 
 app.UseAuthorization();
 
